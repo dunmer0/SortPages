726c5f205c7a7f7b3cef7cf5543448b8ae19b8b8
dunmer0 <popa.catalin88@gmail.com>
2023-11-30 07:46:38 +0200

    Added sorting feature.
    App works fine. maybe it needs some tweaking and code refactoring.
    Still need to see how to make a native image with graalvm.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,37 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Path path = Path.of(System.getProperty("user.dir") + "\\input");
+        Path path = Path.of(System.getProperty("user.dir") + "/input").normalize();
+        Path outputPath = Path.of(System.getProperty("user.dir") + "/output").normalize();
         File dirPath = path.toAbsolutePath().toFile();
-        File filePDF = new File(dirPath + "\\R.F.01_[C2] - Plan fundatii - A1 - 1500.pdf");
-
-        PDDocument pdf = Loader.loadPDF(filePDF);
-        int width = (int) (pdf.getPage(0).getMediaBox().getWidth() * 2.54 / 72.0 * 10);
-        System.out.println(width);
-
-
+        File[] files = new File(String.valueOf(dirPath)).listFiles();
+        assert files != null;
+        Arrays.stream(files).forEach(file -> {
+            try {
+                File filePDF = new File(file.getPath());
+                int width;
+                int height;
+                try (PDDocument pdf = Loader.loadPDF(filePDF)) {
+                    width = (int) (pdf.getPage(0).getMediaBox().getWidth() * 2.54 / 72.0 * 10);
+                    height = (int) (pdf.getPage(0).getMediaBox().getHeight() * 2.54 / 72.0 * 10);
+                }
+                if (!Files.exists(Path.of(outputPath + "/" + height).normalize())) {
+                    Files.createDirectory(Path.of(outputPath + "/" + height));
+                }
+                if (!Files.exists(Path.of(outputPath + "/" + height + "/" + width + "-" + file.getName()).normalize())) {
+                    Files.copy(Path.of(file.getPath()), Path.of(outputPath + "/" + height + "/" + width + "-" + file.getName()).normalize());
+                }
+                Files.delete(Path.of(file.getPath()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
-
-
 }
